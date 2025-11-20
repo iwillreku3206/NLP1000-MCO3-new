11e40b05603bee3dd0c2d30cbb69b643c5a17a5b
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2025-11-20 22:57:46 +0800

    heehee

--- create_sub_corpora.py
+++ create_sub_corpora.py
@@ -41,3 +41,45 @@ corpora['language2_text'] = corpora['temp']
 corpora = corpora.drop(columns=['temp'])
     
 corpora.to_csv("english-to-bicol-corpora.csv")
+
+corpora = parallel_corpora[
+    (parallel_corpora['language1'] == 'bicol') 
+     & (parallel_corpora['language2'] == 'filipino')
+][['language1_text', 'language2_text']]
+
+corpora['temp'] = corpora['language1_text']
+corpora['language1_text'] = corpora['language2_text']
+corpora['language2_text'] = corpora['temp']
+corpora = corpora.drop(columns=['temp'])
+    
+print(f"tgl-bicol: {len(corpora)}")
+
+corpora.to_csv("tagalog-to-bicol-corpora.csv")
+
+corpora = parallel_corpora[
+    (parallel_corpora['language1'] == 'bicol') 
+     & (parallel_corpora['language2'] == 'english')
+][['language1_text', 'language2_text']]
+
+corpora.to_csv("bicol-to-english-corpora.csv")
+
+corpora = parallel_corpora[
+    (parallel_corpora['language1'] == 'english') 
+     & (parallel_corpora['language2'] == 'filipino')
+][['language1_text', 'language2_text']]
+
+corpora['temp'] = corpora['language1_text']
+corpora['language1_text'] = corpora['language2_text']
+corpora['language2_text'] = corpora['temp']
+corpora = corpora.drop(columns=['temp'])
+
+corpora.to_csv("tagalog-to-english-corpora.csv")
+
+corpora = parallel_corpora[
+    (parallel_corpora['language1'] == 'cebuano') 
+     & (parallel_corpora['language2'] == 'english')
+][['language1_text', 'language2_text']]
+
+print(f"ceb-eng: {len(corpora)}")
+
+corpora.to_csv("cebuano-to-english-corpora.csv")
